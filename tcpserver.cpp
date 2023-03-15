ededc63713081320e32498ddd249ab6286faacc0
romka <35815304+r0ka-op@users.noreply.github.com>
2023-03-15 03:31:27 +0300

    v0.1.2
    Нерабочий файл, необходимо решить проблему многопользовательского режима.

--- tcpserver.cpp
+++ tcpserver.cpp
@@ -1,22 +1,20 @@
 #include "tcpserver.h"
-#include <QDebug>
-#include <QCoreApplication>
 
 //using namespace std;
 
 MyTcpServer::~MyTcpServer() // Деструктор
 {
     //mTcpSocket->close();
-    mTcpServer->close();
+    TcpServer->close();
     server_status=0;
 }
 MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent) // Конструктор
 {
-    mTcpServer = new QTcpServer(this);
-    connect(mTcpServer, &QTcpServer::newConnection,
+    TcpServer = new QTcpServer(this);
+    connect(TcpServer, &QTcpServer::newConnection,
             this, &MyTcpServer::slotNewConnection);
 
-    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+    if(!TcpServer->listen(QHostAddress::Any, 33333)){
         qDebug() << "server is not started";
     } else {
         server_status=1;
@@ -24,38 +22,52 @@ MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent) // Конструк�
     }
 }
 
+
+
 void MyTcpServer::slotNewConnection(){
     if(server_status==1){
-        mTcpSocket = mTcpServer->nextPendingConnection(); // Инициализация сокета
-        mTcpSocket->write("I am!!!!!!!!!!!!! NOT !!!!!!!!!!!!!!!!echo server!\r\n"); // Выводим сообщение пользователю
-        
-        connect(mTcpSocket, &QTcpSocket::readyRead,             // коннект на получение сообщения
-                this,&MyTcpServer::slotServerRead);             // при срабатывании запускаем slotServerRead
-        
-        connect(mTcpSocket,&QTcpSocket::disconnected,           // коннект на отключение сокета
-                this,&MyTcpServer::slotClientDisconnected);     // при срабатывании запускаем slotClientDisconnected
-    }
+        QTcpSocket *another_Socket; // Объявление очередного нового сокета
+        qDebug() << TcpSocket << "\r\n"; // TEST LINE
+
+        another_Socket = TcpServer->nextPendingConnection(); // Инициализируем новый сокет
+        another_Socket -> write("I am!!!!!!!!!!!!! NOT !!!!!!!!!!!!!!!!echo server!\r\n"); // Выводим сообщение пользователю
+
+        connect(another_Socket, &QTcpSocket::readyRead,     // коннект на получение сообщения
+                this,&MyTcpServer::slotServerRead);         // при срабатывании запускаем slotServerRead
+
+        connect(another_Socket,&QTcpSocket::disconnected,   // коннект на отключение сокета
+                this,&MyTcpServer::slotClientDisconnected); // при срабатывании запускаем slotClientDisconnected
+
+        TcpSocket[another_Socket->socketDescriptor()] = another_Socket; /* Добавление нового сокета в список сокетов
+                                                                           путём обращения к идентификатору */
+        }
 }
 
 void MyTcpServer::slotServerRead(){
+    QTcpSocket *another_Socket = TcpSocket[TcpServer->socketDescriptor()]; // Инициализация нового сокета
     std::string command;
-    while(mTcpSocket->bytesAvailable()>0)
+    qDebug() << TcpSocket << "\r\n";    // TEST LINE
+    while(another_Socket->bytesAvailable()>0)
     {
-        QByteArray symb = mTcpSocket->readAll();
+        QByteArray symb = another_Socket->readAll();
         command = symb.trimmed().toStdString();
     }
 
     if (command == "auth")
-        mTcpSocket->write("Authorization\r\n");
+        another_Socket -> write("Authorization\r\n");
     else if (command == "reg")
-        mTcpSocket->write("Registration\r\n");
+        another_Socket -> write("Registration\r\n");
+    else if (command == "/log")
+        qDebug() << another_Socket << "\r\n";
     else if (command[0] == '/')
-        mTcpSocket->write("Send command\r\n");
+        another_Socket -> write("Send command\r\n");
     else if (command == "disconnect")
             slotClientDisconnected();
 
 }
 
-void MyTcpServer::slotClientDisconnected(){
-    mTcpSocket->close();
+void MyTcpServer::slotClientDisconnected()
+{
+    QTcpSocket *another_Socket = TcpSocket[TcpServer->socketDescriptor()];
+    another_Socket->close();
 }
